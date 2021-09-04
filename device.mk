77cf1bc8de278424c387811e783be3e44ae04fbd
Cosmin Tanislav <demonsingur@gmail.com>
2021-09-05 01:14:47 +0300

    sapphire: overlay: Allow switching between 60Hz and 120Hz
    Change-Id: I4f328dbdce50e709f30b1f098e71248d72819268

--- device.mk
+++ device.mk
@@ -13,6 +13,7 @@ $(call inherit-product, vendor/xiaomi/sapphire/sapphire-vendor.mk)
 # Overlays
 PRODUCT_PACKAGES += \
     FrameworksResSapphire \
+    SettingsResSapphire \
     SystemUIResSapphire
 
 # Soong namespaces
