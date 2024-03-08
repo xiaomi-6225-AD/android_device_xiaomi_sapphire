f959ecf1bc8918a4b5333797f39335c49a48e791
boedhack99 <boedhack@gmail.com>
2024-03-08 15:44:59 +0700

    sapphire: Inherit from sm6225-common
    Change-Id: I44ba94f9a08649d651e963b53490add5bc8e558a

---
+++ BoardConfig.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from xiaomi sm6225-common
+include device/xiaomi/sm6225-common/BoardConfigCommon.mk
+
+# Inherit from the proprietary version
+include vendor/xiaomi/sapphire/BoardConfigVendor.mk
+
+DEVICE_PATH := device/xiaomi/sapphire
